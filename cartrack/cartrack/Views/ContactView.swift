4ab03fc48ef3fce50f7fc2a81699fe0806437071
Amanda Baret <amanda@rapidfire.digital>
2021-01-16 21:25:50 +0200

    contact view with map

--- cartrack/cartrack/Views/ContactView.swift
+++ cartrack/cartrack/Views/ContactView.swift
@@ -7,47 +7,75 @@
 
 import Foundation
 import SwiftUI
+import MapKit
 
 struct ContactView: View {
     @EnvironmentObject var contactStore: ContactStore
+
+    var region : MKCoordinateRegion {
+
+        let lat =  Double(self.contactStore.contact?.address.geo.lat ?? "") ?? 0
+        let lng =  Double(self.contactStore.contact?.address.geo.lng ?? "") ?? 0
+        return MKCoordinateRegion(center: CLLocationCoordinate2D(latitude:lat, longitude: lng), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    }
+
+    //    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+
 
     var body: some View {
         ZStack {
             Background()
             VStack() {
                 HeaderComponent( title: "Contact")
+
                 ScrollView {
-                VStack(alignment: .leading, spacing: 10) {
-                    Text(self.contactStore.contact?.name ?? "")
-                        .font(.title)
-                        .bold()
-                        .foregroundColor(Color.white)
-                        .lineLimit(nil)
-                        .multilineTextAlignment(.leading)
-                        .fixedSize(horizontal: false, vertical: true)
-                    Text(self.contactStore.contact?.email ?? "")
-                        .bold()
-                        .lineLimit(nil)
-                        .foregroundColor(Color.white)
-                        .multilineTextAlignment(.leading)
-                        .fixedSize(horizontal: false, vertical: true)
-                    Text(self.contactStore.contact?.phone ?? "")
-                        .bold()
-                        .lineLimit(nil)
-                        .foregroundColor(Color.white)
-                        .multilineTextAlignment(.leading)
-                        .fixedSize(horizontal: false, vertical: true)
-                    Text(self.contactStore.contact?.company.name ?? "")
-                        .font(.body)
-                        .bold()
-                        .lineLimit(nil)
-                        .foregroundColor(Color.white)
-                        .multilineTextAlignment(.leading)
-                        .fixedSize(horizontal: false, vertical: true)
-                }
+                    VStack(alignment: .center, spacing: 10) {
+                        Text(self.contactStore.contact?.name ?? "")
+                            .font(.title)
+                            .bold()
+                            .padding(.horizontal)
+                            .foregroundColor(Color.white)
+                            .lineLimit(nil)
+                            .multilineTextAlignment(.center)
+                            .fixedSize(horizontal: false, vertical: true)
+                        Text(self.contactStore.contact?.email ?? "")
+                            .bold()
+                            .lineLimit(nil)
+                            .padding(.horizontal)
+                            .foregroundColor(Color.white)
+                            .multilineTextAlignment(.center)
+                            .fixedSize(horizontal: false, vertical: true)
+                        Text(self.contactStore.contact?.phone ?? "")
+                            .bold()
+                            .lineLimit(nil)
+                            .padding(.horizontal)
+                            .foregroundColor(Color.white)
+                            .multilineTextAlignment(.center)
+                            .fixedSize(horizontal: false, vertical: true)
+                        Text(self.contactStore.contact?.company.name ?? "")
+                            .font(.body)
+                            .bold()
+                            .lineLimit(nil)
+                            .padding(.horizontal)
+                            .foregroundColor(Color.white)
+                            .multilineTextAlignment(.center)
+                            .fixedSize(horizontal: false, vertical: true)
+                    }
+                    MapView(region: self.region)
+                        .frame(width: .infinity, height: UIScreen.main.bounds.height/2, alignment: .top)
+
                 }
             }
         }
+    }
+}
+
+private struct MapView: View {
+    @State var region: MKCoordinateRegion
+
+    var body: some View {
+        Map(coordinateRegion: $region)
+            .padding()
     }
 }
 
